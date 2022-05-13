b2f8ac8648e576e265de025956211b53f65535e0
Bogdan <47000709+BogdanPristinskyi@users.noreply.github.com>
2022-05-12 23:31:31 -0400

    1)Added User registration 2) On main(index) page added temporary navbar 3) List of user page >> Will be modified to have access only by a user with admin role

--- SpringProject/src/main/java/com/example/SpringProject/controller/UserController.java
+++ SpringProject/src/main/java/com/example/SpringProject/controller/UserController.java
@@ -1,4 +1,61 @@
 package com.example.SpringProject.controller;
 
+import com.example.SpringProject.entity.User;
+import com.example.SpringProject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
 public class UserController {
+    @Autowired
+    private UserRepository userRepo;
+
+    @GetMapping("")
+    public String homePage() {
+
+        return "index";
+    }
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+
+        return "signupForm";
+    }
+
+    @PostMapping("/processRegister")
+    public String processRegister(@Valid User user, Errors errors) {
+        // validation
+        if (null != errors && errors.getErrorCount() > 0 ) {
+            return "signupForm";
+        } else if(!user.getRetypePassword().equals(user.getPassword())) {
+
+            errors.rejectValue("retypePassword", "Password and Confirm not the same", null, "Password and Confirm not the same!");
+            return "signupForm";
+        } else{
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            String encodedPassword = passwordEncoder.encode(user.getPassword());
+            user.setPassword(encodedPassword);
+            userRepo.save(user);
+
+            return "registerSuccess";
+        }
+    }
+
+
+
+    @GetMapping("admin/users")
+    public String listUsers(Model model) {
+        List<User> listUsers = userRepo.findAll();
+        model.addAttribute("listUsers", listUsers);
+
+        return "admin/users";
+    }
 }
--- SpringProject/src/main/java/com/example/SpringProject/repository/UserRepository.java
+++ SpringProject/src/main/java/com/example/SpringProject/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.example.SpringProject.repository;
 
-public class UserRepository {
+import com.example.SpringProject.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserRepository  extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    public User findByEmail(String email);
 }
