6351e2d722a7bd18c272f12207fa9474b36222e3
Johan Elder <94501730+johanelder@users.noreply.github.com>
2022-05-18 12:52:06 -0400

    Image
    Lo-fi hack to get an image inside the table using Thymeleaf to concatenate path + filename(form DB)

--- SpringProject/src/main/java/com/example/SpringProject/entity/Hotels.java
+++ SpringProject/src/main/java/com/example/SpringProject/entity/Hotels.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.sql.Blob;
 
 @Entity
 @Table(name = "hotels")
